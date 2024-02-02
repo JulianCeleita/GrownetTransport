27fd543d4ad1f7cc25881e719a73b212f41d807d
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-02-02 08:16:12 -0500

    Se limpia las urls que no se están utilizando

--- src/config/urls.config.js
+++ src/config/urls.config.js
@@ -1,13 +1,4 @@
-export const login = process.env.EXPO_PUBLIC_LOGIN
-export const loginEmployee = process.env.EXPO_PUBLIC_LOGIN_EMPLOYEE
-export const datesAvailables = process.env.EXPO_PUBLIC_DATES_AVAILABLES
-export const deliveryRoutes = process.env.EXPO_PUBLIC_DELIVERY_ROUTES
-export const productsPackingConfig = process.env.EXPO_PUBLIC_PRODUCTS_PACKING
-export const insertPacking = process.env.EXPO_PUBLIC_INSERT_PACKING
-export const productsLoadingConfig = process.env.EXPO_PUBLIC_PRODUCTS_LOADING
-export const insertLoading = process.env.EXPO_PUBLIC_INSERT_LOADING
-export const percentagePacking = process.env.EXPO_PUBLIC_PERCENTAGE_PACKING
-export const percentageLoading = process.env.EXPO_PUBLIC_PERCENTAGE_LOADING
-export const insertShort = process.env.EXPO_PUBLIC_SHORT_INSERT
-export const shortBulkConfig = process.env.EXPO_PUBLIC_SHORT_BULK
-export const shortVanConfig = process.env.EXPO_PUBLIC_SHORT_VAN
+export const login = process.env.EXPO_PUBLIC_LOGIN;
+export const loginEmployee = process.env.EXPO_PUBLIC_LOGIN_EMPLOYEE;
+export const deliveryRoutes = process.env.EXPO_PUBLIC_DELIVERY_ROUTES;
+export const shortVanConfig = process.env.EXPO_PUBLIC_SHORT_VAN;
