89a24ca645c1397d8d4c1cbc9681c5ef07dde125
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-03-01 18:25:07 -0500

    se agrega los productos en transport

---
+++ src/components/ProductsCard.jsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { GlobalStyles, colors } from '../styles/GlobalStyles'
+import { ProductStyles } from '../styles/ProductStyles'
+import { AntDesign } from '@expo/vector-icons'
+
+export function ProductsCard({
+  item,
+}) {
+
+  const isNA = item.state_definitive === 'N/A'
+
+  let backColor = colors.gray;
+  let iconName = 'questioncircleo';
+
+  if (isNA) {
+    backColor = colors.bluePrimary;
+    iconName = '';
+  }
+
+  if (item.state_definitive === 'FULL') {
+    backColor = colors.green;
+    iconName = 'checkcircleo';
+  }
+
+  if (item.state_definitive === 'ND' || item.state_definitive === 'PD') {
+    backColor = colors.orange;
+    iconName = 'arrowright';
+  }
+
+  if (item.state_definitive === 'SHORT') {
+    backColor = colors.danger;
+    iconName = 'closecircleo';
+  }
+
+  return (
+    <View style={{ alignItems: 'center' }} key={item.id}>
+      <View>
+        <View style={[ProductStyles.card, GlobalStyles.boxShadow]}>
+          <View style={ProductStyles.productTittle}>
+            {item.uom != 'Ea' && item.uom != 'Kg' ? (
+              <Text
+                style={[
+                  ProductStyles.tittleCard,
+                  {
+                    textDecorationLine: isNA ? 'line-through' : 'none',
+                  },
+                ]}
+              >
+                {item.name} {item.uom}
+                <Text style={ProductStyles.packingText}>
+                  {' - ' + item.presentationName}
+                </Text>
+              </Text>
+            ) : (
+              <Text
+                style={[
+                  ProductStyles.tittleCard,
+                  {
+                    textDecorationLine: isNA ? 'line-through' : 'none',
+                  },
+                ]}
+              >
+                {item.name} - {item.uom}
+              </Text>
+            )}
+            <View style={ProductStyles.qty}>
+              <Text style={ProductStyles.textCard}>
+                Qty: {item.quantity}
+              </Text>
+              {item.quantity_loading > 0 ? (
+                <>
+                  <Text>
+                    -
+                  </Text>
+                  <Text style={ProductStyles.textCard}>
+                    L: {item.quantity_loading}
+                  </Text>
+                </>
+              ) : null}
+
+            </View>
+          </View>
+          <View style={[ProductStyles.checkBox, {
+            backgroundColor: backColor
+          }]}>
+            {isNA ? (
+              <View>
+                <Text style={ProductStyles.textNA}>N/A</Text>
+              </View>
+            ) : (
+              <AntDesign name={iconName} size={30} color="white" />
+            )}
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
--- src/screens/Login/PinLogin.jsx
+++ src/screens/Login/PinLogin.jsx
@@ -74,6 +74,7 @@ const PinLogin = () => {
           // setSelectedRoute("R4")
           // setSelectedDate('2024-02-27');
           setSelectedDate(new Date().toISOString().slice(0, 10));
+          console.log(response.data, null, 2);
           setEmployeeToken(response.data.token)
           setLoading(false)
         } else {
