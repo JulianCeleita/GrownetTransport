5f94c9b54d203445d70f2aa3896fb09fd0744072
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-03-08 12:34:06 -0500

    Si el estado de la orden es printed, no dejar hacer nada en los productos en short van

--- src/components/ProductsCardShortsVan.jsx
+++ src/components/ProductsCardShortsVan.jsx
@@ -92,6 +92,9 @@ export const ProductsCardShortsVan = ({
 
   const handleClose = () => {
     setModalCard(false)
+    setShowModal(false)
+    setShowModalNA(false)
+    setShowModalRevertNA(false)
   }
 
   useEffect(() => {
@@ -150,12 +153,17 @@ export const ProductsCardShortsVan = ({
   return (
     <View>
       <TouchableOpacity
+        disabled={item.id_stateOrders === 10}
         onPress={() => handlePress(item.id)}
         onLongPress={() => (isNA ? handleRevertNA() : handleDeclareNA())}
         delayLongPress={1000}
       >
         <PanGestureHandler
-          onGestureEvent={(e) => handleGestureEvent(e, item.id)}
+          onGestureEvent={(e) => {
+            if (item.id_stateOrders !== 10) {
+              handleGestureEvent(e, item.id)
+            }
+          }}
           activeOffsetX={[negativeOffset, positiveOffset]}
         >
           <View style={[ProductStyles.card, GlobalStyles.boxShadow]}>
@@ -290,6 +298,7 @@ export const ProductsCardShortsVan = ({
           setStateCardDefault={setStateCardDefault}
           title={item.name + ' will be marked as N/A'}
           text={' Are you sure you want to mark this item as N/A?'}
+          handleClose={handleClose}
         />
       ) : null}
       {showModalRevertNA ? (
@@ -300,6 +309,7 @@ export const ProductsCardShortsVan = ({
           setStateCardDefault={setStateCardDefault}
           title={item.name + ' will enable the product'}
           text={' Are you sure you enable this article?'}
+          handleClose={handleClose}
         />
       ) : null}
 
@@ -311,6 +321,7 @@ export const ProductsCardShortsVan = ({
           setStateCardDefault={setStateCardDefault}
           title={item.name + ' not available'}
           text={' Are you sure you want to mark this item as unavailable?'}
+          handleClose={handleClose}
         />
       ) : null}
     </View>
