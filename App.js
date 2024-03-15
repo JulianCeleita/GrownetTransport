9690249ef7aa1621aa3081c004a4d72be726feba
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2024-03-15 16:46:48 -0500

    Update

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { gestureHandlerRootHOC } from "react-native-gesture-handler";
 import Navigation from "./src/navigation/Navigation";
+import * as Updates from 'expo-updates';
 
 const App = () => {
+
+  const getVersionStore = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+      console.log('update', JSON.stringify(update, null, 2));
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+  useEffect(() => {
+    getVersionStore();
+  }, [])
+
   return <Navigation />;
 };
 
